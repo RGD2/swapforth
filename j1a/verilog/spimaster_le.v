938c84e921b0d6b19c7eea017131e7380fd203f2
Remy Dyer <dye025@csiro.au>
2016-09-24 13:44:28 +1000

    broken - wrong timing data reception

--- j1a/verilog/spimaster_le.v
+++ j1a/verilog/spimaster_le.v
@@ -9,8 +9,10 @@ module spimaster_le (
     output wire SCL,
     input wire MISO);
 
-reg [15:0] sdelay;
-assign running = sdelay[15];
+reg [32:0] sdelay;
+assign running = sdelay[31];
+wire capturing;
+assign capturing = sdelay[32];
 
 reg [15:0] dataout;
 wire MOSI_ = dataout[15];
@@ -28,31 +30,41 @@ SB_IO #(.PIN_TYPE(6'b0000_00)) _miso (
 reg [15:0] datain;
 assign rx = {datain[7:0],datain[15:8]};
 
-always @(posedge clk)
-begin
-    if (running) begin
-        sdelay <= SCL_ ? sdelay : {sdelay[14:0], 1'b0};
-        dataout <= ~SCL_ ? dataout : {dataout[14:0],1'b0};
-        datain <= ~SCL_ ? {datain[14:0], MISO_} : datain;
+reg SCLd;
+
+always @(posedge clk) begin
+    if (capturing) begin
+        datain <= (~SCL_) ? {datain[14:0], MISO_} : datain;
         SCL_ <= ~SCL_;
     end else begin
         SCL_ <= 1'b0;
         datain <= datain;
+    end
+    SCLd <= SCL_;
+end
+
+always @(posedge clk) begin
+    if (running) begin
+        sdelay <= {sdelay[31:0], 1'b0};
+        dataout <= (SCLd) ? {dataout[15:0],1'b0} : dataout ;
+    end else begin
         if (we) begin
-            sdelay <= both ? 16'hffff :  16'hff00 ;
+            sdelay <= both ? 33'h1ffffffff :  33'h1ffff0000 ;
             dataout <= both ? {tx[7:0],tx[15:8]} :  {tx[7:0],8'b0};
         end else begin
-            sdelay <= sdelay;
+            sdelay <=  (capturing) ? {sdelay[31:0], 1'b0} : sdelay ;
             dataout <= dataout;
         end
     end
 end
 
+wire SCL__;
+assign SCL__ = SCL_ & running;
 
 SB_IO #(.PIN_TYPE(6'b0101_01)) _scl (
     .PACKAGE_PIN(SCL),
     .OUTPUT_CLK(clk),
-    .D_OUT_0(SCL_));
+    .D_OUT_0(SCL__));
 
 SB_IO #(.PIN_TYPE(6'b0101_01)) _mosi (
     .PACKAGE_PIN(MOSI),
