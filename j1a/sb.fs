dacd4133585732e9948a375197caa4b14e1e71ef
RGD2 <remydyer@gmail.com>
2017-03-10 10:09:50 +1100

    changed to convenience load sb.fs file

---
+++ j1a/sb.fs
@@ -0,0 +1,9 @@
+\ #include sb.fs on a 'sudo make clean bootstrap j4a' image.
+\ must be done after each reboot.
+\ must never reboot / lose power whilst hydrualics is on!
+\ workaround for image rebuilding being broken
+
+#include j4a-maxrefdes24.fs
+#include mrc-samples.fs
+ini
+
