b18f447b2801c6814763ad02cad15c295334290a
Remy Dyer <remy.dyer@csiro.au>
2017-07-12 14:32:19 +1000

    extra-extra short samples.

---
+++ j1a/ees-samples.fs
@@ -0,0 +1,85 @@
+xak 10 ms 
+sr!0 sr@0
+
+$A989 sr!v
+$A989 sr!v
+$A989 sr!v
+$A989 sr!v
+$A989 sr!v
+$A989 sr!v
+$A989 sr!v
+$A9AD sr!v
+$AA16 sr!v
+$AABA sr!v
+$AB92 sr!v
+$AC96 sr!v
+$ADBD sr!v
+$AEFF sr!v
+$B054 sr!v
+$B1B4 sr!v
+$B316 sr!v
+$B474 sr!v
+$B5C3 sr!v
+$B6FD sr!v
+$B819 sr!v
+$B920 sr!v
+$BA1F sr!v
+$BB17 sr!v
+$BC08 sr!v
+$BCF2 sr!v
+$BDD6 sr!v
+$BEB2 sr!v
+$BF88 sr!v
+$C057 sr!v
+$C120 sr!v
+$C1E2 sr!v
+$C29E sr!v
+$C354 sr!v
+$C404 sr!v
+$C4AE sr!v
+$C552 sr!v
+$C5F1 sr!v
+$C689 sr!v
+$C71C sr!v
+$C7AA sr!v
+$C832 sr!v
+$C8B5 sr!v
+$C933 sr!v
+$C9AC sr!v
+$CA20 sr!v
+$CA8F sr!v
+$CAFA sr!v
+$CB60 sr!v
+$CBC1 sr!v
+\ $CC1E sr!v
+\ $CC76 sr!v
+\ $CCCB sr!v
+\ $CD1B sr!v
+\ $CD67 sr!v
+\ $CDAF sr!v
+\ $CDF4 sr!v
+\ $CC63 sr!v
+$C8BF sr!v
+$C411 sr!v
+$BE59 sr!v
+$B796 sr!v
+$B158 sr!v
+$AB1A sr!v
+$ADDD sr!v
+$AC45 sr!v
+$AAF7 sr!v
+$A9EB sr!v
+$A91B sr!v
+$A87E sr!v
+$A80E sr!v
+$A7C3 sr!v
+$A795 sr!v
+$A77E sr!v
+$A775 sr!v
+$A774 sr!v
+\ updated to fix offset, new beginning of pulse should slowly cross 0.65/12.86mA, also, much shorter.
+\ same as mrc-samples, but another 10% shorter again.
+\ sames as ucc-saples, another 4 removed. (-2ms, about 10% less again )
+\ same as es-samples. "
+68 pl !  \ pulse length
+$A774 ptv ! \ pulse tail value
